6951b92c678a0fc4ba1a571f4792f8a93d76b1f4
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-09-12 16:47:16 -0300

    OK

---
+++ src/pages/Pagina-Pagamento/index.jsx
@@ -0,0 +1,15 @@
+import Cabecalho from "../../components/cabecalho";
+
+
+
+export default function PaginaPagamento() {
+    
+
+    return (
+        <section className="Pagina-Pagamento">
+            
+            <Cabecalho />
+
+        </section>
+    );
+}
