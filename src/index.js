6927a46728513fa5731a2313740677e83e5eeef5
Gabriel <gabrieldesbalmes777@gmail.com>
2023-09-14 22:44:02 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Cadastro from './pages/cadastro';
 import Home from './pages/Home';
 import PaginaPagamento from './pages/Pagina-Pagamento';
 import LoginAdm from './pages/login-adm';
+import EntregaPagamento from './pages/entrega-pagamento';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,6 +20,7 @@ root.render(
         <Route path='/cadastro' element={<Cadastro />} />
         <Route path='/pagamento' element={< PaginaPagamento />} />
         <Route path='/login-adm' element={< LoginAdm />} />
+        <Route path='/entrega' element={< EntregaPagamento />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
