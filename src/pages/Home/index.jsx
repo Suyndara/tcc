4dd4e169bae0a38ed2b0f27cd992d2501b527cfb
Suyndara <souzamatheussilva41@gmail.com>
2023-09-14 21:41:13 -0300

    ok

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,49 +8,48 @@ export default function Home() {
     return(
         <div className='pagina-home'>
             <Cabecalho/>
-            
             <Carrossel/>
 
             <main className='s2'>
                 <article>
-                    <img src="/assets/img/i-truck.png"/>
+                    <img alt='truck' src="/assets/img/i-truck.png"/>
                      <h1>COMPRA SEGURA, ENTREGA RÁPIDA</h1>
                 </article>
 
                 <article>
-                    <img src="/assets/img/i-card.png"/>
+                    <img alt='car' src="/assets/img/i-card.png"/>
                      <h1>PAGAMENTO SIMPLES, FLEXÍVEL E SEGURO</h1>
                 </article>
 
                 <article>
-                    <img src="/assets/img/i-return.png"/>
+                    <img alt='ret' src="/assets/img/i-return.png"/>
                     <h1>TEMOS GARANTIA DE ATÉ 1 ANO</h1>
                 </article>
 
                 <article>
-                    <img src="/assets/img/i-locke.png"/>
+                    <img alt='loc' src="/assets/img/i-locke.png"/>
                     <h1>COMPRE COM SEGURANÇA</h1>
                 </article>
             </main>
 
             <main className='s3'>
                 <article>
-                    <img src="/assets/img/anel.png"/>
+                    <img alt='ring' src="/assets/img/anel.png"/>
                     <button>ANÉIS</button>
                 </article>
 
                 <article>
-                    <img src="/assets/img/colar.png"/>
+                    <img alt='necklace' src="/assets/img/colar.png"/>
                     <button>COLARES</button>
                 </article>
 
                 <article>
-                    <img src="/assets/img/brinco.png"/>
+                    <img alt='brink' src="/assets/img/brinco.png"/>
                     <button>BRINCOS</button>
                 </article>
 
                 <article>
-                    <img src="/assets/img/infantil.png"/>
+                    <img alt='kid' src="/assets/img/infantil.png"/>
                     <button>INFANTIL</button>
                 </article>
             </main>
