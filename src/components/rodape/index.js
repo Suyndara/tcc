1160c16e26e1ef960ed57f534ab77d49793969f6
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-09-17 10:13:39 -0300

    OK

---
+++ src/components/rodape/index.js
@@ -0,0 +1,61 @@
+import Email from '../../assets/img/email.png'
+import Facebook from '../../assets/img/facebook.png'
+import Instagram from '../../assets/img/instagram.png'
+import Twitter from '../../assets/img/twitter.png'
+import Pinterest from '../../assets/img/pinterest.png'
+import Biriri from '../../assets/img/biriri.png'
+import Telefone from '../../assets/img/telefone.png'
+
+
+export default function Rodape() {
+    return (
+        <section className="Rodape">
+            <div className="Duvidas">
+
+                <h1> DÚVIDAS </h1>                    
+
+                <p> Onde esta meu pedido </p>
+                <p> Guia de diamantes </p>
+                <p> Guia de tamanho de aro </p>
+                <p> Gerenciamento de dados pessoais </p>
+                <p> Garantias, trocas e devoluções </p>
+
+            </div>
+            <br/>
+            <div className="Newsletter">
+
+                <h1> NEWSLETTER </h1>
+                <h2> Fique por dentro das novidades da Joyeria Cristallo </h2>
+
+                <div className="Email">
+                    <h3> Assine nosso newsletter </h3>
+                    <img src={Email} alt="email" />
+                </div>
+
+                    <img src={Facebook} alt="facebook" />
+                    <img src={Instagram} alt="instagram" />
+                    <img src={Twitter} alt="twitter" />
+                    <img src={Pinterest} alt="pinterest" />
+
+            </div>
+            <div className="Atendimento">
+
+                <h1> ATENDIMENTO </h1>
+
+                <div>
+                    <h1> +55 11 912345678 </h1>
+                    <img src={Biriri} alt="biriri" />
+                </div>
+                <div>
+                    <h1 >+55 5922-0635 </h1>
+                    <img src={Telefone} alt="telefone" />
+                </div>
+                <div>
+                    <h1> concierge@joyeriacristallo.com.br </h1>
+                    <img src={Email} alt="email" />
+                </div>
+
+            </div>
+        </section>
+    );
+}
