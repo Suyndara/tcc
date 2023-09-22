edb85eecedcb6320773f31f08b59a7a79692bc65
Suyndara <souzamatheussilva41@gmail.com>
2023-09-22 17:49:40 -0300

    ok

--- src/Routes.js
+++ src/Routes.js
@@ -4,8 +4,8 @@ import Home from './pages/Home';
 import Cadastro from './pages/cadastro';
 import EntregaPagamento from './pages/entrega-pagamento';
 import Login from './pages/login/App';
+import Conta from './pages/minha-conta';
 import Edicao from './pages/edicao-adm';
-
 import LoginAdm from './pages/login-adm';
 import HomeAdm from './pages/Pagina-HomeAdm';
 
@@ -15,6 +15,7 @@ export default function Rotas() {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/login' element={<Login />} />
+                <Route path='/minhaconta' element={<Conta/>} />
                 <Route path='/cadastro' element={<Cadastro />} />
                 <Route path='/entrega' element={< EntregaPagamento />} />
                 <Route path='/edicao' element={<Edicao/>} />
---
+++ src/pages/minha-conta/index.jsx
@@ -0,0 +1,57 @@
+import './index.scss';
+import Cabecalho from '../../components/cabecalho'
+import Rodape from '../../components/rodape';
+import { Link } from 'react-router-dom';
+
+
+export default function Conta(){
+
+    return(
+        <div className='pagina-minhaconta'>
+            <Cabecalho/>
+
+            <nav>
+                <article>
+                    <img src="/assets/img/pencil.png" alt="lapis" />
+                </article>
+            </nav>
+
+            <main>
+                <article className='opcoes'>
+                        <p>Minha conta</p>
+                        <p>Meus Pedidos</p>
+                        <p>Crédito em Loja</p>
+                        <p>Devoluções</p>
+                        <p>Detalhes da Conta</p>
+                        <p>Meus Endereços</p>
+                        <p>Atualizar Senha</p>
+
+                        <section>
+                            <h1>Sair</h1>
+                            <Link to='/'>
+                                <img src="/assets/img/icone-de-sair.png" alt="" />
+                            </Link>
+                        </section>
+                </article>
+
+                <aside>
+                    <span>
+                        <input type="text" placeholder='Nome completo'/>
+                        <input type="text" placeholder='Email'/>
+                        <input type="text" placeholder='Número de telefone'/>
+                        <input type="text" placeholder='Endereço'/>
+
+                        <section>
+                            <input type="text" placeholder='Cidade'/>
+                            <input type="text" placeholder='Estado'/>                                
+                        </section>                        
+
+                        <input type="text" placeholder='Senha'/>
+                    </span>
+                </aside>
+            </main>
+
+            <Rodape/>
+        </div>
+    )
+}
