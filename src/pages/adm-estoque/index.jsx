41a47cac9eddfd010380d02259f057331af149bb
Suyndara <souzamatheussilva41@gmail.com>
2023-10-11 01:51:14 -0300

    ok

--- src/pages/adm-estoque/index.jsx
+++ src/pages/adm-estoque/index.jsx
@@ -2,7 +2,7 @@ import './index.scss';
 import Lateral from '../../components/lateralAdm';
 import Opcoes from '../../components/opcoes';
 import CabecalhoAdm from '../../components/cabecalhoAdm';
-
+import Anel from '../../assets/img/anelouro.png';
 export default function Estoque() {
     
     return(
@@ -31,31 +31,27 @@ export default function Estoque() {
                     </article>
 
                     <article className='sas'>
-                    <p>Joias</p>
-                            <p>Aneis</p>
-                            <p>Relogios</p>
-                            <p>Correntes</p>
-                            <p>Brincos</p>
-                            <p>Colares</p>
-                            <p>Pedras</p>  <p>Joias</p>
-                            <p>Aneis</p>
-                            <p>Relogios</p>
-                            <p>Correntes</p>
-                            <p>Brincos</p>
-                            <p>Colares</p>
-                            <p>Pedras</p>  <p>Joias</p>
-                            <p>Aneis</p>
-                            <p>Relogios</p>
-                            <p>Correntes</p>
-                            <p>Brincos</p>
-                            <p>Colares</p>
-                            <p>Pedras</p>  <p>Joias</p>
-                            <p>Aneis</p>
-                            <p>Relogios</p>
-                            <p>Correntes</p>
-                            <p>Brincos</p>
-                            <p>Colares</p>
-                            <p>Pedras</p>
+                        <aside>
+                            <div className="itens">
+                                <img src={Anel} alt="ringold" />
+
+                                <div className='infos'>
+                                    <div className='ifs1'>
+                                        <h1>Anel solitário de ouro 18k com diamantes de 3 pontos</h1>
+
+                                        <p>Contem no estoque:</p>
+                                        <p>QTD no estoque:</p>
+                                        <p>Promoção:</p>
+                                    </div>     
+
+                                    <div className='ifs'>
+                                        <p>Ok</p>
+                                        <p>5</p>
+                                        <p>X</p>
+                                    </div>            
+                                </div>
+                            </div>
+                        </aside>
                     </article>
                 </section>
             </main>
