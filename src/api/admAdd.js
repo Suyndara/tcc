afdfad4e44c5ca4839f9ce1a679f986283423f9f
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-10-13 18:12:16 -0300

    OK

--- src/api/admAdd.js
+++ src/api/admAdd.js
@@ -6,6 +6,26 @@ const api = axios.create({
 
 
 
+export async function CadastrarJoia(nome, preco, estoque, disponivel, composicao, detalhes, categoria, subcategoria) {
+    const resp = await api.post('/produto', {
+        nome: nome,
+        preco: preco,
+        categoria: categoria,
+        subcategoria: subcategoria,
+        estoque: estoque,
+        composicao: composicao,
+        detalhes: detalhes,         
+        disponivel: disponivel
+    });
+
+    console.log(CadastrarJoia);
+
+    return resp.data;
+};
+
+
+
+
 export async function AdicionarImg(imagem) {
     const formData = new FormData();
     formData.append('Produto', imagem)
@@ -18,21 +38,4 @@ export async function AdicionarImg(imagem) {
     });
 
     return resp.status;
-}
-
-
-export async function CadastrarJoia(nome, preco, estoque, disponivel, composicao, detalhes, categoria, subCategoria) {
-    const resp = await api.post('/produto', {
-            nome: nome,
-            preco: preco,
-            disponivel: disponivel,
-            estoque: estoque,
-            composicao: composicao,
-            detalhes: detalhes,
-            categoria: categoria,
-            subcategoria: subCategoria
-    });
-
-    return resp.data;
-}
-
+};
--- src/pages/adm-edicao/index.jsx
+++ src/pages/adm-edicao/index.jsx
@@ -8,30 +8,27 @@ import CabecalhoAdm from '../../components/cabecalhoAdm';
 
 import { ToastContainer, toast } from 'react-toastify';
 import { useState } from 'react';
-import { CadastrarJoia, AdicionarImg } from '../../api/admAdd.js'
+import { CadastrarJoia, AdicionarImg } from '../../api/admAdd'
 
 
 export default function Edicao(){
 
-    const [ nome, setNome ] = useState('');
+    const [ nome, setNome ] = useState();
     const [ preco, setPreco ] = useState();
     const [ categoria, setCategoria ] = useState();
-    const [ subCategoria, setSubCategoria ] = useState();
+    const [ subcategoria, setSubCategoria ] = useState();
     const [ estoque, setEstoque ] = useState();
-    const [ composicao, setComposicao ] = useState('');
-    const [ detalhes, setDestalhes ] = useState('');
+    const [ composicao, setComposicao ] = useState();
+    const [ detalhes, setDestalhes ] = useState();
     const [ disponivel, setDisponivel ] = useState(false);
-
-    const [ imagem, setImg ] = useState();
 
 
 
     async function AdicionarProduto() {
         try {
             
-            const resp = await CadastrarJoia( nome, preco, disponivel, estoque, composicao, detalhes, categoria, subCategoria );
-
-            toast.done('Filme Cadastrado');
+            const resp = await CadastrarJoia( nome, preco, disponivel, estoque, composicao, detalhes, categoria, subcategoria );
+            console.log(CadastrarJoia);
 
         } catch (error) {
             toast.error(error.message);
@@ -59,14 +56,14 @@ export default function Edicao(){
                             <input type="text" placeholder='Nome' value={nome} onChange={e => setNome(e.target.value)} />
                             <input type="text" placeholder='Valor' value={preco} onChange={e => setPreco(e.target.value)}/>
                             <input type="text" placeholder='Categoria' value={categoria} onChange={e => setCategoria(e.target.value)}/>
-                            <input type="text" placeholder='Sub-Categoria' value={subCategoria} onChange={e => setSubCategoria(e.target.value)}/>
-                            <input type="number" placeholder='Qtd. Inicial' value={estoque} onChange={e => setEstoque(e.target.value)}/>
+                            <input type="text" placeholder='Sub-Categoria' value={subcategoria} onChange={e => setSubCategoria(e.target.value)}/>
+                            <input type="number" placeholder='Estoque' value={estoque} onChange={e => setEstoque(e.target.value)}/>
                             <textarea placeholder='Composição'value={composicao} onChange={e => setComposicao(e.target.value)} />
                             <textarea placeholder='Detalhes' value={detalhes} onChange={e => setDestalhes(e.target.value)}/>
 
                             <div>
 
-                                <input type="checkbox" value={disponivel} onChange={e => setDisponivel(e.target.checked)}/>
+                                <input type="checkbox" checked={disponivel} onChange={e => setDisponivel(e.target.checked)}/>
                                 <p> Disponivel </p>
 
                             </div>
@@ -81,7 +78,7 @@ export default function Edicao(){
 
                                <div className='bt-img'>
                                     <button>
-                                        <img src="/assets/img/plus.png" alt="ms" value={imagem} onChange={e => setImg(e.target.value)}/>
+                                        <img src="/assets/img/plus.png" alt="ms" />
                                     </button>
                                 </div> 
                             </article>
