bd7450e31551650595c2c392fc35846c54c6d9dc
Suyndara <souzamatheussilva41@gmail.com>
2023-10-17 14:29:29 -0300

    ok

--- src/pages/adm-home/index.jsx
+++ src/pages/adm-home/index.jsx
@@ -43,7 +43,7 @@ export default function HomeAdm() {
 
                         </span>
 
-                        <p>Cadastrar</p>
+                        <p>Cadastrarr</p>
                     </article>
 
                     <Link to='/login-adm' className='volta'>
