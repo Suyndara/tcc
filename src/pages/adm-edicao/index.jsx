f6ccd984d2cd97f3f8fc703f087bceb30b15848e
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-10-18 16:10:50 -0300

    ok

--- src/pages/adm-edicao/index.jsx
+++ src/pages/adm-edicao/index.jsx
@@ -69,7 +69,7 @@ export default function Edicao(){
                             <input type="text" placeholder='Valor' value={preco} onChange={e => setPreco(e.target.value)}/>
                             <input type="text" placeholder='Categoria' value={categoria} onChange={e => setCategoria(e.target.value)}/>
                             <input type="text" placeholder='Sub-Categoria' value={subcategoria} onChange={e => setSubCategoria(e.target.value)}/>
-                            <input type="text" placeholder='Estoque' value={estoque} onChange={e => setEstoque(e.target.value)}/>
+                            <input type="number" placeholder='Estoque' value={estoque} onChange={e => setEstoque(e.target.value)}/>
                             <textarea placeholder='Composição'value={composicao} onChange={e => setComposicao(e.target.value)} />
                             <textarea placeholder='Detalhes' value={detalhes} onChange={e => setDestalhes(e.target.value)}/>
 
