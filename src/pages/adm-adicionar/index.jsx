a0107e61cf981abef0f2c1de90a749b3b9db2e42
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-10-18 22:51:55 -0300

    OK

--- src/pages/adm-adicionar/index.jsx
+++ src/pages/adm-adicionar/index.jsx
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import Lateral from '../../components/lateralAdm';
 import CabecalhoAdm from '../../components/cabecalhoAdm';
+import add from '../../assets/img/adicionar.png'
 
 
 import { ToastContainer, toast } from 'react-toastify';
@@ -29,6 +30,7 @@ export default function Edicao(){
             const resp = await cadastrarJoia( nome, preco, categoria, subcategoria, estoque, composicao, detalhes, disponivel );
             toast.success('Sucesso');
 
+            console.log(resp);
         } catch (error) {
             toast.error(error.response.data.erro);
         }
@@ -38,11 +40,11 @@ export default function Edicao(){
 
 
     return(
-        <div className='pagina-edicao'>
-            <Lateral/>
+        <div className='pagina-anexagem-produto'>
             <ToastContainer />
+            <Lateral/>
 
-            <main>
+            <main className='main-anexagem-produto'>
                 <CabecalhoAdm/>
 
                 <div className='s1'>
@@ -52,34 +54,39 @@ export default function Edicao(){
                     </article>
 
                     <article className='cont-2'>
-                        <section>    
-                            <input type="text" placeholder='Nome' value={nome} onChange={e => setNome(e.target.value)} />
-                            <input type="text" placeholder='Valor' value={preco} onChange={e => setPreco(e.target.value)}/>
-                            <input type="text" placeholder='Categoria' value={categoria} onChange={e => setCategoria(e.target.value)}/>
-                            <input type="text" placeholder='Sub-Categoria' value={subcategoria} onChange={e => setSubCategoria(e.target.value)}/>
-                            <input type="number" placeholder='Estoque' value={estoque} onChange={e => setEstoque(e.target.value)}/>
-                            <textarea placeholder='Composição'value={composicao} onChange={e => setComposicao(e.target.value)} />
-                            <textarea placeholder='Detalhes' value={detalhes} onChange={e => setDestalhes(e.target.value)}/>
+                        <section>
 
-                            <div>   
+                            <div className="sep-1">
 
-                                <input type="checkbox" checked={disponivel} onChange={e => setDisponivel(e.target.checked)}/>
-                                <p> Disponivel </p>
+                                <input type="text" placeholder='Nome' value={nome} onChange={e => setNome(e.target.value)} />
+                                <input type="text" placeholder='Valor' value={preco} onChange={e => setPreco(e.target.value)}/>
+                                <input type="text" placeholder='Categoria' value={categoria} onChange={e => setCategoria(e.target.value)}/>
+                                <input type="text" placeholder='Sub-Categoria' value={subcategoria} onChange={e => setSubCategoria(e.target.value)}/>
+                                <input type="number" placeholder='Estoque' value={estoque} onChange={e => setEstoque(e.target.value)}/>
+                                <div>   
+                                    <input type="checkbox" checked={disponivel} onChange={e => setDisponivel(e.target.checked)}/>
+                                    <label> Disponivel </label>
+                                </div>
 
                             </div>
 
-                            <button onClick={ AdicionarProduto }> ADICIONAR </button>
-                            
-                        </section>
+                            <div className="sep-2">
 
-                        <aside>
-                            <article >
-                                <h1>imagens.png</h1>
-                            </article>
-                        </aside>
+                                <textarea placeholder='Composição'value={composicao} onChange={e => setComposicao(e.target.value)} />
+                                <textarea placeholder='Detalhes' value={detalhes} onChange={e => setDestalhes(e.target.value)}/>
+
+                                    <article >
+                                        <img src={add} alt="Adicionar +" />
+                                    </article>
+                            </div>
+
+                            <button onClick={ AdicionarProduto }> ADICIONAR </button>
+
+                        </section>
                     </article>
                 </div>
             </main>
+
         </div>
-    )
-}
+    );
+};
