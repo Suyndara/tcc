74247fd481c9d2963b2be3fbac808d35d7213395
Suyndara <souzamatheussilva41@gmail.com>
2023-10-24 15:31:43 -0300

    ok

--- src/components/rodape/index.js
+++ src/components/rodape/index.js
@@ -1,66 +1,78 @@
-import Email from '../../assets/img/email.png';
-import Facebook from '../../assets/img/facebook.png';
-import Instagram from '../../assets/img/instagram.png';
-import Twitter from '../../assets/img/twitter.png';
-import Pinterest from '../../assets/img/pinterest.png';
-import Biriri from '../../assets/img/biriri.png';
-import Telefone from '../../assets/img/telefone.png';
+import Email from "../../assets/img/email.png";
+import Facebook from "../../assets/img/facebook.png";
+import Instagram from "../../assets/img/instagram.png";
+import Twitter from "../../assets/img/twitter.png";
+import Pinterest from "../../assets/img/pinterest.png";
+import Biriri from "../../assets/img/biriri.png";
+import Telefone from "../../assets/img/telefone.png";
 
-import './index.scss';
-
-
+import "./index.scss";
 
 export default function Rodape() {
-    return (
-        <section className="Rodape">
-            <div className="Duvidas">
+  return (
+    <section className="Rodape">
+      <main className="rdp">
+        <div className="Duvidas">
+          <h1> DÚVIDAS </h1>
 
-                <h1> DÚVIDAS </h1>                    
+          <p> Onde esta meu pedido </p>
+          <p> Guia de diamantes </p>
+          <p> Guia de tamanho de aro </p>
+          <p> Gerenciamento de dados pessoais </p>
+          <p> Garantias, trocas e devoluções </p>
+        </div>
+        <div className="Newsletter">
+          <h1> NEWSLETTER </h1>
+          <h2> Fique por dentro das novidades da Joyeria Cristallo </h2>
 
-                <p> Onde esta meu pedido </p>
-                <p> Guia de diamantes </p>
-                <p> Guia de tamanho de aro </p>
-                <p> Gerenciamento de dados pessoais </p>
-                <p> Garantias, trocas e devoluções </p>
+          <div className="Email">
+            <h3> Assine nosso newsletter </h3>
+            <img src={Email} alt="email" />
+          </div>
 
-            </div>
-            <div className="Newsletter">
+          <div className="Redes-Sociais">
+            <img src={Facebook} alt="facebook" />
+            <img src={Instagram} alt="instagram" />
+            <img src={Twitter} alt="twitter" />
+            <img src={Pinterest} alt="pinterest" />
+          </div>
+        </div>
+        <div className="Atendimento">
+          <h1> ATENDIMENTO </h1>
 
-                <h1> NEWSLETTER </h1>
-                <h2> Fique por dentro das novidades da Joyeria Cristallo </h2>
+          <div>
+            <h2> +55 11 912345678 </h2>
+            <img src={Biriri} alt="biriri" />
+          </div>
+          <div>
+            <h2>+55 5922-0635 </h2>
+            <img src={Telefone} alt="telefone" />
+          </div>
 
-                <div className="Email">
-                    <h3> Assine nosso newsletter </h3>
-                    <img src={Email} alt="email" />
-                </div>
+          <div>
+            <h2> concierge@joyeriacristallo.com.br </h2>
+            <img src={Email} alt="email" />
+          </div>
+        </div>
+      </main>
 
-                    <div className="Redes-Sociais">
-                        <img src={Facebook} alt="facebook" />
-                        <img src={Instagram} alt="instagram" />
-                        <img src={Twitter} alt="twitter" />
-                        <img src={Pinterest} alt="pinterest" />
-                    </div>
+      <div className="Atendimento-2">
+          <h1> ATENDIMENTO </h1>
 
-            </div>
-            <div className="Atendimento">
+          <div>
+            <h2> +55 11 912345678 </h2>
+            <img src={Biriri} alt="biriri" />
+          </div>
+          <div>
+            <h2>+55 5922-0635 </h2>
+            <img src={Telefone} alt="telefone" />
+          </div>
 
-                <h1> ATENDIMENTO </h1>
-
-                <div>
-                    <h2> +55 11 912345678 </h2>
-                    <img src={Biriri} alt="biriri" />
-                </div>
-                <div>
-                    <h2>+55 5922-0635 </h2>
-                    <img src={Telefone} alt="telefone" />
-                </div>
-                
-                <div>
-                    <h2> concierge@joyeriacristallo.com.br </h2>
-                    <img src={Email} alt="email" />
-                </div>
-
-            </div>
-        </section>
-    );
+          <div>
+            <h2> concierge@joyeriacristallo.com.br </h2>
+            <img src={Email} alt="email" />
+          </div>
+        </div>
+    </section>
+  );
 }
