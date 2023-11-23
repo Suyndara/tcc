580bdc7eb832f79bafa1dfdfed24c96384c554d9
SidneyFalcaoo <sidneyoliveirafalcaooo@gmail.com>
2023-11-23 15:33:48 -0300

    ok

--- src/pages/entrega-pagamento/index.jsx
+++ src/pages/entrega-pagamento/index.jsx
@@ -1,31 +1,58 @@
 import './index.scss';
+
+
 import Cabecalho from '../../components/cabecalho';
 import Rodape from '../../components/rodape';
 import Resumo from '../../components/resumo';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { toast } from 'react-toastify'
 import storage from 'local-storage'
-import { concluirPedido, inserirItensPedido } from '../../api/UsuarioAdd';
+
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify'
+import { concluirPedido, inserirItensPedido, InserirEndereco } from '../../api/UsuarioAdd';
 
 export default function EntregaPagamento() {
-    const [pedido, setPedido] = useState({})
+    const [pedido, setPedido] = useState({});
+
+
+    const [ nome, setNome ] = useState();
+    const [ enderecoRua, setEnderecoRua ] = useState();
+    const [ cep, setCep ] = useState();
+    const [ numeroCasa, setNumeroCasa ] = useState();
+    const [ complemento, setComplemento ] = useState();
+    const [ bairro, setBairro ] = useState();
+    const [ cidade, setCidade ] = useState();
+    const [ estado, setEstado ] = useState();
 
 
     useEffect(() => {
         setPedido(storage('usuario-pedido'))
 
         // eslint-disable-next-line
-    }, [])
+    }, []);
+
+
+
+    async function adicaoEndereco() {
+        try {
+            
+            const resp = InserirEndereco(nome, enderecoRua, cep, numeroCasa, complemento, bairro, cidade, estado)
+
+        } catch (error) {
+            if (error.response)
+            toast.error(error.response.data.erro);
+        }
+    }
 
 
     async function finalizarPedido() {
         try{
-            console.log('oi');
+            adicaoEndereco();
             const id = storage('usuario-logado').cliente_id
+
+
             const resp = await concluirPedido({cliente: id, total: storage('usuario-pedido').total})
             const respItens = await inserirItensPedido(storage('usuario-pedido').carrinho, resp.id)
-            alert('Pedido finalizado')
+            toast.done('Pedido finalizado');
         }
         catch(err){
             if(err.response)
@@ -33,7 +60,7 @@ export default function EntregaPagamento() {
             else
                 toast.error(err.message)
         }
-    }
+    };
 
     return (
 
@@ -47,22 +74,22 @@ export default function EntregaPagamento() {
 
                     <div className="informacoes">
                     
-                        <input type="text" placeholder='Nome Completo' />
-                        <input type="text" placeholder='Endereço' />
+                        <input type="text" placeholder='Nome Completo' value={nome} onChange={e => setNome(e.target.value)}/>
+                        <input type="text" placeholder='Endereço' value={enderecoRua} onChange={e => setEnderecoRua(e.target.value)}/>
 
                         <div className='cep'>
-                            <input type="text" placeholder='CEP' />
-                            <input type='text' placeholder='Número' />
+                            <input type="text" placeholder='CEP'  value={cep} onChange={e => setCep(e.target.value)}/>
+                            <input type='text' placeholder='Número'  value={numeroCasa} onChange={e => setNumeroCasa(e.target.value)}/>
                         </div>
 
                         <div className='bairro'>
-                            <input type='text' placeholder='Complemento' />
-                            <input type='text' placeholder='Bairro' />
+                            <input type='text' placeholder='Complemento'  value={complemento} onChange={e => setComplemento(e.target.value)}/>
+                            <input type='text' placeholder='Bairro'  value={bairro} onChange={e => setBairro(e.target.value)}/>
                         </div>
 
                         <div className='cidade'>
-                            <input type='text' placeholder='Cidade' />
-                            <input type='text' placeholder='Estado' />
+                            <input type='text' placeholder='Cidade'  value={cidade} onChange={e => setCidade(e.target.value)}/>
+                            <input type='text' placeholder='Estado' value={estado} onChange={e => setEstado(e.target.value)} />
                         </div>
                     </div>
 
