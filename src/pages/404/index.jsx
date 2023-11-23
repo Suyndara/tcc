173d831614451e1a239a97866e8acb572e0cf72a
Suyndara <souzamatheussilva41@gmail.com>
2023-11-23 15:05:24 -0300

    ok

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,10 +1,17 @@
 import './index.scss';
+import Cabecalho from '../../components/cabecalho';
 
 export default function NotFound() {
     
     return(
         <div className='naoencontrado'>
-            
+            <Cabecalho/>
+
+            <main>
+                <article>
+                    <h1>Página não encontrada</h1>
+                </article>
+            </main>
         </div>
     )
 }
